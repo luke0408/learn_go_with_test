d587f7b564c6e03cf5db3db8320cf64d5a006083
luke0408 <zjvl324@gmail.com>
2024-07-11 00:24:21 +0900

    ♻️ Refactor: range 적용

--- 4_array_and_slices/sum.go
+++ 4_array_and_slices/sum.go
@@ -3,8 +3,9 @@ package main
 func Sum(numbers [5]int) int {
 	sum := 0
 
-	for i := 0; i < 5; i++ {
-		sum += numbers[i]
+	// range: 인덱스와 값을 반환 (인덱스는 사용하지 않을 때 _로 무시)
+	for _, number := range numbers {
+		sum += number
 	}
 
 	return sum
