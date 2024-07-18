c9303fc3478ee91b9e0e4117bf3e8b108c9938e4
luke0408 <zjvl324@gmail.com>
2024-07-19 01:07:33 +0900

    ✨ Feat: Triangle 구현

--- 5_structs_method_interface/shape.go
+++ 5_structs_method_interface/shape.go
@@ -11,6 +11,11 @@ type Circle struct {
 	Radius float64
 }
 
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
 type Shape interface {
 	Area() float64
 }
@@ -26,4 +31,8 @@ func (r Rectangle) Area() float64 {
 
 func (c Circle) Area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) * 0.5
 }
