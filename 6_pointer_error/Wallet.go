acdc55b095895c3268a29bc7b5e7052a4b3ef664
luke0408 <zjvl324@gmail.com>
2024-07-19 01:38:56 +0900

    ✨ Feat: Stringer 적용

--- 6_pointer_error/Wallet.go
+++ 6_pointer_error/Wallet.go
@@ -1,5 +1,11 @@
 package main
 
+import "fmt"
+
+type Stringer interface {
+	String() string
+}
+
 type Bitcoin int
 
 type Wallet struct {
@@ -13,4 +19,8 @@ func (w *Wallet) Deposit(amount Bitcoin) {
 func (w *Wallet) Balance() Bitcoin {
 	// (*w).balance라고 쓰지 않아도 자동 역참조가 가능함
 	return w.balance
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
 }
