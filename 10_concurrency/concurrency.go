3dd6afa685218eab20ba1cb2ec355ca559b76bb0
luke0408 <zjvl324@gmail.com>
2024-07-27 03:45:01 +0900

    ♻️ Refactor: gorutien 및 channel 사용

--- 10_concurrency/concurrency.go
+++ 10_concurrency/concurrency.go
@@ -1,12 +1,26 @@
 package concurrency
 
 type WebsiteChecker func(string) bool
+type result struct {
+	string
+	bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
+	resultsChannel := make(chan result)
 
 	for _, url := range urls {
-		results[url] = wc(url)
+		go func(u string) { // goroutine
+			// 채널에 결과를 보냄
+			resultsChannel <- result{u, wc(u)} // channel
+		}(url)
+	}
+
+	for i := 0; i < len(urls); i++ {
+		// 채널에서 결과를 받음
+		r := <-resultsChannel // channel
+		results[r.string] = r.bool
 	}
 
 	return results
