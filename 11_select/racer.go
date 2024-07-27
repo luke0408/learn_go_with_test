ef1369597bca7da573f7f22bb771dabe67b0e183
luke0408 <zjvl324@gmail.com>
2024-07-27 13:08:31 +0900

    ♻️ Refactor: select 구조 도입

--- 11_select/racer.go
+++ 11_select/racer.go
@@ -2,23 +2,22 @@ package main
 
 import (
 	"net/http"
-	"time"
 )
 
 func Racer(a, b string) (winner string) {
-	aDuration := measureResponseTime(a)
-	bDuration := measureResponseTime(b)
-
-	if aDuration < bDuration {
+	select {
+	case <-ping(a):
 		return a
+	case <-ping(b):
+		return b
 	}
-
-	return b
 }
 
-func measureResponseTime(url string) time.Duration {
-	start := time.Now()
-	http.Get(url)
-	duration := time.Since(start)
-	return duration
+func ping(b string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(b)
+		close(ch)
+	}()
+	return ch
 }
