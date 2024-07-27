99983217261109e2d8e463fa9fc1b8d4e16506a1
luke0408 <zjvl324@gmail.com>
2024-07-27 16:42:15 +0900

    :recycle: Refactor: switch 문으로 변경

--- 12_reflection/walk.go
+++ 12_reflection/walk.go
@@ -2,11 +2,28 @@ package main
 
 import "reflect"
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func walk(x interface{}, fn func(input string)) {
+	// Reflect를 사용하여 x의 타입을 가져온다.
 	val := reflect.ValueOf(x)
 
 	for i := 0; i < val.NumField(); i++ {
 		field := val.Field(i)
-		fn(field.String())
+
+		switch field.Kind() {
+		case reflect.String:
+			fn(field.String())
+		case reflect.Struct:
+			walk(field.Interface(), fn)
+		}
 	}
 }
