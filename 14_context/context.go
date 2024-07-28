34fbbb0b725ecd604d7a47131aa268eeaaae03aa
luke0408 <zjvl324@gmail.com>
2024-07-28 13:11:27 +0900

    ✨ Feat: happy path server 구현

---
+++ 14_context/context.go
@@ -0,0 +1,16 @@
+package _4_context
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type Store interface {
+	Fetch() string
+}
+
+func Server(store Store) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, store.Fetch())
+	}
+}
