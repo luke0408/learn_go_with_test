580e42738696714a4d8c3439c5e4a51790222833
luke0408 <zjvl324@gmail.com>
2024-07-28 17:01:03 +0900

    ♻️ Refactor: context 사용 방식 수정

--- 14_context/context_test.go
+++ 14_context/context_test.go
@@ -9,24 +9,40 @@ import (
 )
 
 type SpyStore struct {
-	response  string
-	cancelled bool
+	response string
+	t        *testing.T
 }
 
-func (s *SpyStore) Fetch() string {
-	time.Sleep(100 * time.Millisecond)
-	return s.response
+func (s *SpyStore) Fetch(ctx context.Context) (string, error) {
+	data := make(chan string, 1)
 
-}
+	go func() {
+		var result string
+		for _, c := range s.response {
+			select {
+			case <-ctx.Done():
+				s.t.Log("spy store got cancelled")
+				return
+			default:
+				time.Sleep(10 * time.Millisecond)
+				result += string(c)
+			}
+		}
+		data <- result
+	}()
 
-func (s *SpyStore) Cancel() {
-	s.cancelled = true
+	select {
+	case <-ctx.Done():
+		return "", ctx.Err()
+	case res := <-data:
+		return res, nil
+	}
 }
 
 func TestServer(t *testing.T) {
 	t.Run("returns data from store", func(t *testing.T) {
 		data := "hello, world"
-		store := &SpyStore{response: data}
+		store := &SpyStore{response: data, t: t}
 		svr := Server(store)
 
 		request := httptest.NewRequest(http.MethodGet, "/", nil)
@@ -36,30 +52,6 @@ func TestServer(t *testing.T) {
 
 		if response.Body.String() != data {
 			t.Errorf("got %q, want %q", response.Body.String(), data)
-		}
-
-		if store.cancelled {
-			t.Error("it should not have cancelled the store")
-		}
-	})
-
-	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
-		data := "hello, world"
-		store := &SpyStore{response: data}
-		svr := Server(store)
-
-		request := httptest.NewRequest(http.MethodGet, "/", nil)
-
-		cancelingCtx, cancel := context.WithCancel(request.Context())
-		time.AfterFunc(5*time.Millisecond, cancel)
-		request = request.WithContext(cancelingCtx)
-
-		response := httptest.NewRecorder()
-
-		svr.ServeHTTP(response, request)
-
-		if !store.cancelled {
-			t.Error("store was not told to cancel")
 		}
 	})
 }
